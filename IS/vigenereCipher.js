241310296c36e735044402c61e1f1d94d94f620f
param <param772003@gmail.com>
2024-05-17 13:25:50 +0530

    modifications in Vigenere Cipher

--- IS/vigenereCipher.js
+++ IS/vigenereCipher.js
@@ -2,14 +2,11 @@ let plainText = "universal";
 let key = "college";
 let cipherText = "";
 
-if(key.length < plainText.length)
+for(let i = 0; i < plainText.length; i++)
 {
-    for(let i = 0; i < plainText.length; i++)
-    {
-        let asciiValue = ((plainText.charCodeAt(i) - 97 + key.charCodeAt(i % key.length) - 97) % 26) + 97;
-        let cipherLetter = String.fromCharCode(asciiValue);
-        cipherText += cipherLetter;
-    }
+    let asciiValue = ((plainText.charCodeAt(i) - 97 + key.charCodeAt(i % key.length) - 97) % 26) + 97;
+    let cipherLetter = String.fromCharCode(asciiValue);
+    cipherText += cipherLetter;
 }
 
 console.log("Ciphertext: ", cipherText);
